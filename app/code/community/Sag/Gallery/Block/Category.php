f6a0d802feed0d817ddf38fc88e06adacb512817
Yevhenii Harbuzniak <fnostric@gmail.com>
2014-03-30 01:34:14 +0200

    changes

---
+++ app/code/community/Sag/Gallery/Block/Category.php
@@ -0,0 +1,52 @@
+<?php
+class Sag_Gallery_Block_Category extends Mage_Core_Block_Template
+{
+	public function _prepareLayout()
+    {
+		return parent::_prepareLayout();
+    }
+    
+    public function getCategory()     
+    { 
+        if (!$this->hasData('category')) {
+            $this->setData('category', Mage::getModel('gallery/category')->getCollection());
+        }
+        return $this->getData('category');
+    }
+	
+	
+	/**
+	 * Resize Image proportionally and return the resized image url
+	 *
+	 * @param string $imageName         name of the image file
+	 * @param integer|null $width       resize width
+	 * @param integer|null $height      resize height
+	 * @param string|null $imagePath    directory path of the image present inside media directory
+	 * @return string               full url path of the image
+	 */
+	public function resizeImage($imageName, $width=NULL, $height=NULL, $imagePath=NULL)
+	{      
+		$imagePath = str_replace("/", DS, $imagePath);
+		$imagePathFull = Mage::getBaseDir('media') . DS . $imagePath . DS . $imageName;
+		 
+		if($width == NULL && $height == NULL) {
+			$width = 100;
+			$height = 100;
+		}
+		$resizePath = $width . 'x' . $height;
+		$resizePathFull = Mage::getBaseDir('media') . DS . $imagePath . DS . $resizePath . DS . $imageName;
+				 
+		if (file_exists($imagePathFull) && !file_exists($resizePathFull)) {
+			$imageObj = new Varien_Image($imagePathFull);
+			$imageObj->constrainOnly(TRUE);
+			$imageObj->keepAspectRatio(TRUE);
+			$imageObj->resize($width,$height);
+			$imageObj->save($resizePathFull);
+		}
+				 
+		$imagePath=str_replace(DS, "/", $imagePath);
+		$img = Mage::getBaseUrl("media") . $imagePath . "/" . $resizePath . "/" . $imageName;
+		return str_replace(DS, "/", $img);
+	}
+
+}
